a681ffb70ae6aba6ef9647f3f96e82ef968f2fdd
Agustin Ituarte <agustin242536@gmail.com>
2022-10-10 21:25:45 -0300

    Corregido algunas funciones y se agrego una para calcular automaticamente el valor de la cuadrilla

--- script.js
+++ script.js
@@ -11,22 +11,23 @@ function crearGrid(cuboPequeño, numCubos) {
         div1.setAttribute('style', `width: ${cuboPequeño}px; height: ${cuboPequeño}px; border: 1px solid black`);
         container.appendChild(div1);
 
-        //dibujar(div1);
-    }
+            div1.addEventListener('mouseover', () => { // Agrega atributo de background-color cada vez que el mouse pasa por encima de una cuadrado.
+                div1.setAttribute('style', `width: ${cuboPequeño}px; height: ${cuboPequeño}px; border: 1px solid black; background-color: black`);
+            })
+        }     
 }
 
-function dibujar(div1) { // Agrega atributo de background-color cada vez que el mouse pasa por encima de una cuadrado.
-    div1.addEventListener('mouseover', () => {
-        div1.setAttribute('style', `width: ${cubopequeño}px; height: ${cubopequeño}px; border: 1px solid black; background-color: black`);
+function calcularCuadrilla(){
+    btn.addEventListener('click', () => {
+        let gridValor = prompt('Ingresa tamaño de la cuadricula:(maximo: 100x100)');
+        if (gridValor > 100) {
+            alert("Cuadricula demasiado grande");
+        } else {
+            let cuboPequeño = 530 / gridValor - 2;
+            let numCubos = gridValor * gridValor;
+            crearGrid(cuboPequeño, numCubos);
+        }
     })
 }
 
-btn.addEventListener('click', () => {
-    let gridValor = prompt('Ingresa tamaño de la cuadricula');
-    let cuboPequeño = 530 / gridValor - 2;
-    let numCubos = gridValor * gridValor;
-    crearGrid(cuboPequeño, numCubos);
-
-    dibujar(div1);
-})
-
+calcularCuadrilla()
